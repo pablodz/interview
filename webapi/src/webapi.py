3fd3af1ed65dabfddae5ca8cff97c4e72910a925
pablodiaz <pablo.diazv@pucp.edu.pe>
2021-07-16 23:42:39 -0500

    Update Dockerfile, db-service.yml, and 6 more files...

--- webapi/src/webapi.py
+++ webapi/src/webapi.py
@@ -1,61 +1,129 @@
 # Imports
-from pymongo import MongoClient
-from flask import (Flask, jsonify)
-from flask_restful import (Api, Resource)
+from flask import Flask, jsonify, url_for, redirect, request
+from flask_pymongo import PyMongo
+from flask_restful import Api, Resource
 
+from _constants import HOST_WEBAPI, PORT_WEBAPI, APP_URL
+from index import Index
 
+# Configs
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/TestDB"
+mongo_flask = PyMongo(app)
+
+
+# Class
+
+class Dog(Resource):
+    """
+    Dog class that has attributes to register a new dog
+    """
+
+    def get(self,
+            id_registration=None,
+            dog_owner=None
+            ):
+        data = []
+
+        if id_registration:
+            studnet_info = mongo_flask.testCollection.testCollection.find_one(
+                {"id_registration": id_registration},
+                {"_id": 0}
+            )
+            if studnet_info:
+                return jsonify({
+                    "status": "ok",
+                    "data": studnet_info,
+                })
+            else:
+                return {"response": "no dog found for {}".format(id_registration)}
+
+        elif dog_owner:
+            cursor = mongo_flask.testCollection.testCollection.find(
+                {"dog_owner": dog_owner},
+                {"_id": 0}
+            ).limit(5)
+            for dog in cursor:
+                dog['url'] = APP_URL + \
+                    url_for('dogs') + \
+                    "/" + \
+                    dog.get('id_registration')
+                data.append(dog)
+
+            return jsonify({"dog_owner": dog_owner, "response": data})
+
+        else:
+            cursor = mongo_flask.testCollection.testCollection.find(
+                {},
+                {"_id": 0, "update_time": 0}
+            ).limit(5)
+
+            for dog in cursor:
+                print(dog)
+                dog['url'] = APP_URL + \
+                    url_for('dogs') +\
+                    "/" +\
+                    dog.get('id_registration')
+                data.append(dog)
+
+            return jsonify({"response": data})
+
+    def post(self):
+        data = request.get_json()
+        if not data:
+            data = {"response": "ERROR"}
+            return jsonify(data)
+        else:
+            id_registration = data.get('id_registration')
+            if id_registration:
+                if mongo_flask.testCollection.testCollection.find_one({"id_registration": id_registration}):
+                    return {"response": "dog already exists."}
+                else:
+                    mongo_flask.testCollection.testCollection.insert(data)
+            else:
+                return {"response": "id_registration number missing"}
+
+        return redirect(url_for("dogs"))
+
+    def put(self,
+            id_registration
+            ):
+        data = request.get_json()
+        mongo_flask.testCollection.testCollection.update(
+            {'id_registration': id_registration},
+            {'$set': data}
+        )
+        return redirect(url_for("dogs"))
+
+    def delete(self,
+               id_registration
+               ):
+        mongo_flask.testCollection.testCollection.remove({
+            'id_registration': id_registration
+        })
+        return redirect(url_for("dogs"))
+
+
+# Endpoints
 api = Api(app)
-
-
-client = MongoClient("mongodb://db:27017")
-db = client["TestDB"]
-testCollection = db["testCollection"]
-
-
-# Class Hello
-class Hello(Resource):
-    def get(self):
-        """
-        Returns a Hello jsonified
-        """
-
-        responseJson = {
-            "status": "200",
-            "message": "Hello there, this is Hello Class json response."
-        }
-
-        return jsonify(responseJson)
-
-# Class Homepage
-
-
-class HomePage(Resource):
-    def get(self):
-        """
-        Returns a JSON with GET endpoints
-        """
-
-        responseJson = {
-            "message": ("HomePage: here you can check all endpoints",
-                        "listed on this site dockerized."),
-            "status": "200",
-            "hello": "/hello",
-        }
-
-        return jsonify(responseJson)
-
-
-# HomePage
-api.add_resource(HomePage, '/')
-# HelloPage
-api.add_resource(Hello, '/hello')
-
+# api.add_resource(HomePage, '/') # HomePage
+api.add_resource(Index,
+                 "/",
+                 endpoint="index")
+api.add_resource(Dog(),
+                 "/v1/api",
+                 endpoint="dogs")
+api.add_resource(Dog(),
+                 "/v1/api/<string:id_registration>",
+                 endpoint="id_registration")
+api.add_resource(Dog(),
+                 "/v1/api/dog_owner/<string:dog_owner>",
+                 endpoint="dog_owner")
 
 if __name__ == "__main__":
-    # Run the main app
+    # Running main app
     app.run(
         debug=True,
-        host='0.0.0.0',
-        port=8501
+        host=HOST_WEBAPI,
+        port=PORT_WEBAPI
     )
